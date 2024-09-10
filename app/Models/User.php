0fc3c518efcd3eaa3b1be1b5b2bcc555c8e3ae9d
Sadra_Zargari <sadra.zr.81@gmail.com>
2024-09-10 14:58:41 +0330

    Register,Login,Logout(ueser)

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,9 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fullname',
+        'username',
         'email',
+        'mobile',
+        'avatar',
         'password',
+        'confirm_code',
     ];
 
     /**
