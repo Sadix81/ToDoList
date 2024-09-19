0c6a318f00b07ff2d2b42449457f3d9c72119549
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2024-09-19 16:54:05 +0330

    Login testing

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function test_auth_user_login(){
+        $data = [
+            'username' => 'sadra',
+            // 'email' => 'sadra@gmail.com',
+            'password' => 'S@dra2091'
+        ];
+
+        $response = $this->postJson('api/v1/user/login' , $data);
+
+        //get json
+        $response->assertJsonStructure(['__token__']);
+        $response->assertStatus(200);
+    }
+}
