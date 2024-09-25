8a36b803880cf4bf54ae756ec04416940f0ef064
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2024-09-26 01:30:11 +0330

    correct

--- app/Http/Controllers/V1/Auth/AuthController.php
+++ app/Http/Controllers/V1/Auth/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
         if ($accessToken) {
             return response()->json(['message' => __('messages.user.auth.login.success'), '__token__' => $accessToken], 200);
         }
-        return response()->json(['message' => __('messages.user.auth.login.failed')], 500);
+        return response()->json(['message' => __('messages.user.auth.login.failed')], 403);
     }
 
     public function logout(){
