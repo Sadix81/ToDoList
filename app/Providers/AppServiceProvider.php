f269e00b8dad4de909ebc29bc14559f7228fb5b5
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2024-10-01 12:48:46 +0330

    group CRUD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,10 @@ namespace App\Providers;
 
 use App\Repositories\Auth\AuthRepository;
 use App\Repositories\Auth\AuthRepositoryInterface;
+use App\Repositories\Group\GroupRepository;
+use App\Repositories\Group\GroupRepositoryInterface;
 use App\Repositories\Task\TaskRepository;
 use App\Repositories\Task\TaskRepositoryInterface;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -18,7 +19,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
-        $this->app->bind(TaskRepositoryInterface::class , TaskRepository::class);
+        $this->app->bind(TaskRepositoryInterface::class, TaskRepository::class);
+        $this->app->bind(GroupRepositoryInterface::class, GroupRepository::class);
     }
 
     /**
@@ -26,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $token_expire_token_time = (int)env('SESSION_LIFETIME', '60');
+        $token_expire_token_time = (int) env('SESSION_LIFETIME', '60');
         $token_expire_token_time = $token_expire_token_time ? $token_expire_token_time : 60;
         Passport::personalAccessTokensExpireIn(now()->addDays($token_expire_token_time));
     }
