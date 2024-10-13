fe87f2b6ff65a31f2cc35381b7ad0ad76f9366be
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2024-10-13 18:44:48 +0330

    correct category

--- app/Http/Resources/Task/IndexTaskResource.php
+++ app/Http/Resources/Task/IndexTaskResource.php
@@ -17,7 +17,7 @@ class IndexTaskResource extends JsonResource
             'finished_at' => $this->finished_at,
             'priority' => $this->priority,
             'status' => $this->status,
-            'category' => $this->categories->pluck('id'),
+            'category' => $this->categories->pluck('title'),
         ];
     }
 }
