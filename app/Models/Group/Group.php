12e035b41c3e49987befd9743baddfeda812130d
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2024-10-15 17:47:27 +0330

    correct subtask

--- app/Models/Group/Group.php
+++ app/Models/Group/Group.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Group;
 
+use App\Models\Task\Task;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -18,5 +19,10 @@ class Group extends Model
     public function users()
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
     }
 }
