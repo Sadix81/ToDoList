d61624fcdf46738ee137bfe419fa1e13d88ab24e
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2024-10-22 01:32:10 +0330

    fix subtask

--- app/Repositories/Subtask/SubtaskRepositoryInterface.php
+++ app/Repositories/Subtask/SubtaskRepositoryInterface.php
@@ -4,7 +4,7 @@ namespace App\Repositories\Subtask;
 
 interface SubtaskRepositoryInterface
 {
-    // public function index();
+    public function index($task);
 
     public function store($task , $request);
 
