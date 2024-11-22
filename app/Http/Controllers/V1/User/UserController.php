6678ba6d76268c297e2a3865f66444d3c900709c
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2024-11-22 23:39:51 +0330

    search user

---
+++ app/Http/Controllers/V1/User/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\V1\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\SearchUserRequest;
+use App\Http\Resources\User\SearchUserResource;
+use App\Repositories\User\UserRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    private $userRepo;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepo = $userRepository;
+    }
+
+    public function user_list(SearchUserRequest $request){
+        $user = Auth::id();
+
+        if (! $user) {
+            return 'عدم دسترسی';
+        }
+        $users = $this->userRepo->user_list($request);
+
+        return SearchUserResource::collection($users);
+    }
+}
